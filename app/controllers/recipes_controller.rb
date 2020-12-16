260823ebbca93351113b0f10e9f432f257923994
racross1 <racross1@gmail.com>
2020-12-16 15:31:31 -0800

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,12 +5,41 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
+    
   end
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
+
+    # @recipes.ingredients.build(title: 'Recipe 1')
+    # @recipes.ingredients.build(title: 'Recipe 2')
+    # @person.addresses.build(address_type: 'work')
+    # @person.addresses.build(address_type: 'home')
   end
 
   def create
+    @recipe = Recipe.create(title: recipe_params[:title])
+    recipe_attr_1 = recipe_params[:ingredients_attributes]["0"]
+    recipe_attr_2 = recipe_params[:ingredients_attributes]["1"]
+
+    recipe_1 = Ingredient.create(recipe_id: @recipe.id, name: recipe_attr_1[:name], quantity: recipe_attr_1[:quantity])
+    recipe_2 = Ingredient.create(recipe_id: @recipe.id, name: recipe_attr_2[:name], quantity: recipe_attr_2[:quantity])
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, 
+      ingredients_attributes: [
+        :name, 
+        :quantity
+      ]
+    )
+  end 
+
+
 end
+
